29ba70cc4897daddbf7832749f55b7a5fdb6b8bd
mihon <mihon@zencoders.ru>
2015-07-08 23:43:48 +0300

    Convert edentations into spaces

--- challenge_me.rb
+++ challenge_me.rb
@@ -1,11 +1,11 @@
 # encoding: utf-8
 
 def solution text, comment_with
-	comment_with.each do |e| 
-		matcher = "(#{e}.*?(\\n|$))"
-		text = text.sub(Regexp.new(matcher), "\n").gsub(/\s$/,'')
-	end
-	puts text.inspect	
+  comment_with.each do |e| 
+    matcher = "(#{e}.*?(\\n|$))"
+    text = text.sub(Regexp.new(matcher), "\n").gsub(/\s$/,'')
+  end
+  puts text.inspect 
 end
 
 result = solution("apples, pears # and bananas\ngrapes\nbananas !apples                           ", ["#", "!"])
